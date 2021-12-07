bd2154a85d1c1b97626bbfe5aae01df222d11e56
PETER WILSON <PETERWILSON@COLLEGE.HARVARD.EDU>
2021-12-07 11:19:20 -0500

    upload

---
+++ fantasybasketball/application.py
@@ -0,0 +1,516 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+from helpers import apology, login_required, player_stats, lookup
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///basketball.db")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+
+    # Render template to index html
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/leagueselect", methods=["GET", "POST"])
+@login_required
+def leagueselect():
+
+    # Join leagues table and league_user table to select which league to select in
+    league = db.execute(
+        "SELECT * FROM leagues WHERE id in (SELECT league_id FROM league_user WHERE user_id = ?)", session["user_id"])
+
+    # Creating a session for league_id
+    if request.method == "POST":
+        session["league_id"] = request.form.get('league')
+
+        # Ensure league was selected
+        if not session["league_id"]:
+            return apology("must select league", 403)
+
+        # Redirect to players
+        return redirect("/players")
+
+    # Return leagueselect html
+    return render_template("leagueselect.html", league=league)
+
+
+@app.route("/players", methods=["GET", "POST"])
+@login_required
+def players():
+    """Get players stats."""
+
+    # Create empty dict for stats to put data into
+    stats = []
+
+    # Select relevant data for stats and league to pick players
+    stats = db.execute(
+        "SELECT * FROM stats WHERE player_id NOT IN (SELECT player_id from user_player WHERE league_id = ?)", int(session['league_id']))
+    league = db.execute(
+        "SELECT * FROM leagues WHERE id in (SELECT league_id FROM league_user WHERE user_id = ?)", session["user_id"])
+
+    # # SQL command used to add player id into stats
+    # players = db.execute("SELECT id, team_id, first_name, last_name FROM players")
+    # for i in range(len(players)):
+    #     db.execute("UPDATE stats SET player_id = ? WHERE name = ?", players[i]['id'], players[i]['first_name'] + ' ' + players[i]['last_name'] )
+    # # Realized we needed team id, so this is sql to copy over team id to stats
+    # players = db.execute("SELECT id, team_id FROM players")
+    # for i in range(len(players)):
+    #     db.execute("UPDATE stats SET team_id = ? WHERE player_id = ?", players[i]['team_id'], players[i]['id'])
+
+    # Sort based on preferences given
+    if request.method == "POST":
+
+        if request.form.get('stat') == 'salaryasc':
+            stats = db.execute(
+                "SELECT * FROM stats WHERE player_id NOT IN (SELECT player_id from user_player WHERE league_id = ?) ORDER BY salary", int(session['league_id']))
+
+        if request.form.get('stat') == 'salarydesc':
+            stats = db.execute(
+                "SELECT * FROM stats WHERE player_id NOT IN (SELECT player_id from user_player WHERE league_id = ?) ORDER BY salary DESC", int(session['league_id']))
+
+        if request.form.get('stat') == 'points':
+            stats = db.execute(
+                "SELECT * FROM stats WHERE player_id NOT IN (SELECT player_id from user_player WHERE league_id = ?) ORDER BY points DESC", int(session['league_id']))
+
+    # Query database for user's cash
+    rows = db.execute("SELECT cash FROM league_user WHERE user_id = ? AND league_id = ?",
+                      session["user_id"], int(session["league_id"]))
+    cash = rows[0]['cash']
+
+    # players = player_stats()
+
+    # Return players
+    return render_template("players.html", stats=stats, league=league, cash=cash)
+
+    # for i in range(len(players)):
+    #     players[i]['data'].append(name[i])
+    # db.execute("INSERT INTO stats (name, position, team, points, assists, rebounds, steals, blocks, turnovers, salary) VALUES (?,?,?,?,?,?,?,?,?,?)",
+    #             players[i]['data'][1]['first_name'] + ' ' + players[i]['data'][1]['last_name'], players[i]['data'][1]['position'], players[i]['data'][1]['team'], players[i]['data'][0]['pts'],
+    #             players[i]['data'][0]['ast'], players[i]['data'][0]['reb'], players[i]['data'][0]['stl'], players[i]['data'][0]['blk'], players[i]['data'][0]['turnover'],
+    #             players[i]['data'][1]['salary'])
+    # for i in range(len(stats)):
+    #     print(stats[i]['name'])
+
+    #     name["player"] = row["first_name"]
+    # for row in players["data"]:
+    #     length = len(row)
+    #     for i in range(length):
+    #         name = ['first_name'][i]
+
+
+@app.route("/add", methods=["GET", "POST"])
+@login_required
+def add():
+
+    # POST
+    if request.method == "POST":
+
+        # Query for relevant player information
+        player_id = request.form.get('id')
+        players = db.execute("SELECT * FROM players WHERE id = ?", player_id)
+
+        # Extract cash of player chosen
+        cost = players[0]["salary"]
+
+        # Get user's cash balance
+        rows = db.execute("SELECT cash FROM league_user WHERE user_id = ? AND league_id = ?",
+                          session["user_id"], int(session["league_id"]))
+        if not rows:
+            return apology("missing user")
+        cash = rows[0]["cash"]
+
+        # Ensure user can afford player
+        if cash < cost:
+            return apology("can't afford")
+        count = db.execute("SELECT COUNT(player_id) FROM user_player WHERE user_id = ? and league_id = ?",
+                           session["user_id"], int(session["league_id"]))
+
+        # Set limit to number of players brought
+        if count[0]["COUNT(player_id)"] == 7:
+            return apology("Too many players")
+
+        # Insert player, user and league information to user_player
+        db.execute("INSERT into user_player (user_id, player_id, league_id) VALUES (?,?,?)", session["user_id"],
+                   player_id, int(session['league_id']))
+
+        # Deduct cash
+        db.execute("UPDATE league_user SET cash = cash - ? WHERE user_id = ? and league_id = ?",
+                   cost, session["user_id"], int(session["league_id"]))
+
+        # Return myteam
+        return redirect("/myteam")
+
+
+@app.route("/delete", methods=["GET", "POST"])
+@login_required
+def delete():
+
+    if request.method == "POST":
+        player_id = request.form.get('id')
+
+        players = db.execute("SELECT salary FROM players WHERE id = ?", player_id)
+
+        # Get cost of player
+        print(players)
+        cost = players[0]['salary']
+
+        # Get user's cash balance
+        rows = db.execute("SELECT cash FROM league_user WHERE user_id = ? and league_id = ?",
+                          session["user_id"], int(session["league_id"]))
+        if not rows:
+            return apology("missing user")
+        cash = rows[0]['cash']
+
+        # Drop players user wants to
+        db.execute("DELETE FROM user_player WHERE player_id = ? AND league_id = ?", player_id, int(session["league_id"]))
+
+        # Update the user's cash as a result
+        db.execute("UPDATE league_user SET cash = cash + ? WHERE user_id = ? and league_id = ?",
+                   cost, session["user_id"], int(session["league_id"]))
+
+        return redirect("/myteam")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password") or not request.form.get("confirmation"):
+            return apology("must provide password and confirmation", 400)
+
+        # Ensure password matches confirmation
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+
+        elif len(request.form.get("password")) < 8:
+            return apology("passwords must be 8 characters long", 400)
+
+        elif request.form.get("password").isalpha():
+            return apology("passwords must contain at least 1 number or special character", 400)
+
+        # Ensure username does not exist in database
+        name = db.execute("SELECT username FROM users WHERE username = ?", request.form.get("username"))
+        if name:
+            return apology("username is already taken", 400)
+
+        # Store username in database
+        db.execute("INSERT INTO users (username, hash) VALUES (?,?)",
+                   request.form.get("username"), generate_password_hash(request.form.get("password")))
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # Return Register
+    return render_template("register.html")
+
+
+@app.route("/create", methods=["GET", "POST"])
+@login_required
+def create():
+    """Create league"""
+
+    # POST
+    if request.method == "POST":
+
+        # Ensure league name was submitted
+        if not request.form.get("name"):
+            return apology("must provide league name", 400)
+
+        # Check league name conditions
+        for i in range(len(request.form.get("name"))):
+            print(request.form.get("name")[i])
+            if not request.form.get("name")[i].isalpha() and request.form.get("name")[i] != " ":
+                return apology("must provide alphabetic league name", 400)
+
+        # Ensure number of teams submitted
+        if not request.form.get("teams"):
+            return apology("invalid team count")
+        elif not request.form.get("teams").isdigit():
+            return apology("invalid team count")
+        teams = int(request.form.get("teams"))
+        if teams < 4 or teams > 10:
+            return apology("range of teams go from 4 to 10")
+
+        # Check to see if league name is already taken
+        x = db.execute("SELECT league_name FROM leagues WHERE league_name = ?", request.form.get("name"))
+        if len(x) > 0:
+            return apology("That league name is already taken, please choose another", 400)
+
+        # Record league information
+        db.execute("INSERT INTO leagues (league_name, league_max) VALUES(?, ?)", request.form.get("name"), teams)
+
+        # Display creation success
+        return render_template("created.html"), {"Refresh": "2; url=/leagues"}
+
+    # GET
+    else:
+        return render_template("create.html")
+
+
+@app.route("/join", methods=["GET", "POST"])
+@login_required
+def join():
+    """Join league"""
+
+    # POST
+    if request.method == "POST":
+
+        # Query for league id's
+        l_id = db.execute("""SELECT * FROM leagues WHERE league_name = ?""", request.form.get("symbol"))
+
+        # Ensure league was selected
+        if not l_id:
+            return apology("select league", 400)
+
+        # Query for league name
+        pID = session["user_id"]
+        name = db.execute("SELECT league_name FROM league_user WHERE user_id = ? and league_name = ?",
+                          pID, request.form.get("symbol"))
+
+        # Ensure user is not in league
+        if name:
+            return apology("aready joined league", 400)
+
+        # Update transaction table as well
+        db.execute("INSERT INTO league_user (league_id, user_id, league_name) VALUES (?,?,?)",
+                   l_id[0]["id"], pID, request.form.get("symbol"))
+
+        # Deposit cash
+        db.execute("UPDATE leagues SET league_current = league_current + 1 WHERE league_name = ?",
+                   request.form.get("symbol"))
+
+        # Display portfolio
+        flash("Joined!")
+        return redirect("/")
+
+    # GET
+    else:
+
+        # Show available leagues
+        league = db.execute("SELECT * FROM leagues WHERE league_current < league_max")
+
+        # Display Join
+        return render_template("join.html", league=league)
+
+
+@app.route("/leagues")
+@login_required
+def leagues():
+    """pathway to creating or joining league"""
+
+    # Display leagues
+    return render_template("leagues.html")
+
+
+# Same leagueselect design as before but for myteam
+@app.route("/leagueselect2", methods=["GET", "POST"])
+@login_required
+def leagueselect2():
+
+    league = db.execute(
+        "SELECT * FROM leagues WHERE id in (SELECT league_id FROM league_user WHERE user_id = ?)", session["user_id"])
+
+    if request.method == "POST":
+        session["league_id"] = request.form.get('league')
+
+        # Ensure league was selected
+        if not session["league_id"]:
+            return apology("must select league", 403)
+
+        return redirect("/myteam")
+
+    return render_template("leagueselect2.html", league=league)
+
+
+@app.route("/myteam", methods=["GET", "POST"])
+@login_required
+def myteam():
+    """shows user's team for each league"""
+
+    # Create empty dict for points and select relevant info for leagues
+    points = {}
+    league = db.execute(
+        "SELECT * FROM leagues WHERE id in (SELECT league_id FROM league_user WHERE user_id = ?)", session["user_id"])
+
+    # SQL query to keep track of plyer's fantasy points and total fantasy points by team
+    myteam = db.execute("SELECT DISTINCT game_id, name, player_id, position FROM fantasy_points WHERE player_id IN (SELECT player_id FROM user_player WHERE user_id = ? AND league_id = ?) GROUP BY player_id",
+                        session["user_id"], int(session["league_id"]))
+    point = db.execute("SELECT DISTINCT game_id, name, total_points FROM fantasy_points WHERE player_id IN (SELECT player_id FROM user_player WHERE user_id = ? AND league_id = ?)",
+                       session["user_id"], int(session["league_id"]))
+
+    # Iterate through point to extract player fantasy points
+    for i in range(len(point)):
+        player = point[i]['name']
+        if player in points:
+            points[player] += point[i]['total_points']
+        else:
+            points[player] = point[i]['total_points']
+
+    # Calculate total fantasy points for user
+    total = sum(points.values())
+
+    # Query database for user's cash
+    rows = db.execute("SELECT cash FROM league_user WHERE user_id = ? AND league_id = ?",
+                      session["user_id"], int(session["league_id"]))
+    cash = rows[0]['cash']
+
+    # Record fantasy_points in league_user
+    db.execute("UPDATE league_user SET fantasy_points = ? WHERE user_id = ? AND league_id = ?",
+               total, session["user_id"], int(session["league_id"]))
+
+    # Return myteam
+    return render_template("myteam.html", league=league, myteam=myteam, points=points, total=total, cash=cash)
+
+
+@app.route("/update", methods=["GET", "POST"])
+@login_required
+def update():
+    if request.method == "POST":
+
+        # Used lookup helper function to extract stats
+        lookup()
+
+        # Iterate through each player to update stats accordingly
+        stats = db.execute("SELECT name, player_id, position FROM stats")
+        for i in range(len(stats)):
+            db.execute("UPDATE fantasy_points set name = ? WHERE player_id = ?", stats[i]["name"], stats[i]["player_id"])
+            db.execute("UPDATE fantasy_points set position = ? WHERE player_id = ?", stats[i]["position"], stats[i]["player_id"])
+
+        return redirect("/myteam")
+
+# Same league select design as before
+
+
+@app.route("/leagueselect3", methods=["GET", "POST"])
+@login_required
+def leagueselect3():
+
+    league = db.execute(
+        "SELECT * FROM leagues WHERE id IN (SELECT league_id FROM league_user WHERE user_id = ?)", session["user_id"])
+
+    if request.method == "POST":
+        session["league_id"] = request.form.get('league')
+
+        # Ensure league was selected
+        if not session["league_id"]:
+            return apology("must select league", 403)
+
+        return redirect("/leaderboards")
+
+    return render_template("leagueselect3.html", league=league)
+
+
+@app.route("/leaderboards")
+@login_required
+def leaderboards():
+    """shows leaderboards for each league user is in"""
+
+    # Select relevant league information
+    league = db.execute(
+        "SELECT * FROM leagues WHERE id in (SELECT league_id FROM league_user WHERE user_id = ?)", session["user_id"])
+
+    # Query database for league rankings based on league
+    standings = db.execute(
+        "SELECT username,fantasy_points FROM users JOIN league_user ON users.id = league_user.user_id WHERE league_id = ? ORDER BY fantasy_points DESC", int(session["league_id"]))
+
+    # Return leaderboards
+    return render_template("leaderboards.html", league=league, standings=standings)
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
---
+++ fantasybasketball/helpers.py
@@ -0,0 +1,129 @@
+import os
+import requests
+import urllib.parse
+import json
+from datetime import date
+from flask import redirect, render_template, request, session
+from functools import wraps
+from cs50 import SQL
+
+db = SQL("sqlite:///basketball.db")
+
+# Apology helper function
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def player_stats():
+    """Look up stats for player."""
+
+    # Contact API
+    text = []
+    player_ids = []
+    ids = []
+    try:
+        # this is how we got the initial players for our database
+        player_ids = db.execute("SELECT id FROM players")
+        # print(len(player_ids))
+        for i in range(len(player_ids)):
+            ids.append(player_ids[i]['id'])
+            # print(ids)
+        # player_ids = db.execute("SELECT id FROM players SORT BY ?",  )
+        # print(ids[0])
+        # print(len(ids))
+        for i in range(len(ids)):
+            url = f"https://www.balldontlie.io/api/v1/season_averages?season=2019&player_ids[]={ids[i]}"
+            response = requests.get(url)
+            response.raise_for_status()
+            text.append(response.json())
+            # print(text)
+    except requests.RequestException:
+        return None
+
+    hi = json.dumps(text, indent=4)
+    data = json.loads(hi)
+    # print(text)
+
+    return data
+
+
+def lookup():
+    ids = []
+    try:
+        player_ids = db.execute("SELECT id FROM players")
+        # print(player_ids)
+        today = date.today()
+        # this was used for our demo and the model we left in
+        #Since we used a free API just be careful spamming update-only get a limited
+        #number of calls per minute
+        string = 'per_page=100&start_date=2019-12-07&end_date=2019-12-12'
+        # this commented line is what should update the games daily if you click update
+        # string = f'per_page=100&dates={today}'
+        # API call getting all player stats in our db;
+        for i in range(len(player_ids)):
+            string += f"&player_ids[]={player_ids[i]['id']}"
+            query = 'https://www.balldontlie.io/api/v1/stats?' + string
+            url = f"{query}"
+            # print(url)
+        response = requests.get(url).json()
+        page = 1
+        # loop used to iterate through each page that the API brought back
+        while True:
+            url = f'https://www.balldontlie.io/api/v1/stats?page={page}' + '&' + string
+            response = requests.get(url).json()
+            ids.append(response)
+            if type(response['meta']['next_page']) != int:
+                last_page = response['meta']['total_count'] - ((response['meta']['current_page'] - 1) * 100)
+                break
+            page += 1
+        # print(ids)
+    except requests.RequestException:
+        return None
+    # iterate through every page before last page, see documentation for retrieving data from API calls
+    # insert data into fantasy_points
+    for i in range(len(ids) - 1):
+        for j in range(100):
+            # print(ids[i]['data'][j]['player']['id'])
+            db.execute("INSERT INTO fantasy_points (player_id, total_points, game_id) VALUES (?,?,?)",
+                       ids[i]['data'][j]['player']['id'], ids[i]['data'][j]['pts'] + (2 * ids[i]['data'][j]['ast']) +
+                       (2 * ids[i]['data'][j]['reb']) + (2 * ids[i]['data'][j]['stl']) + (2 * ids[i]['data'][j]['blk'])
+                       - (2 * ids[i]['data'][j]['turnover']), ids[i]['data'][j]['game']['id'])
+    # print(last_page)
+    # iterate through last page of data
+    # insert data into fantasy points
+    last = response['meta']['current_page'] - 1
+    for i in range(last_page):
+        # print(ids[last]['data'][i]['pts'])
+        db.execute("INSERT INTO fantasy_points (player_id, total_points, game_id) VALUES (?,?,?)",
+                   ids[last]['data'][i]['player']['id'], ids[last]['data'][i]['pts'] + (2 * ids[last]['data'][i]['ast']) +
+                   (2 * ids[last]['data'][i]['reb']) + (2 * ids[last]['data'][i]['stl']) + (2 * ids[last]['data'][i]['blk'])
+                   - (2 * ids[last]['data'][i]['turnover']), ids[last]['data'][i]['game']['id'])
+
+    return
